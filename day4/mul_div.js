095aa5e01d646a1291cd83a7d4d0a3a4172c463f
Pralhadrajparajuli <pralhad.078bct046@acem.edu.np>
2026-09-11 09:37:48 +0545

    multiply and divide with input and buttom

---
+++ day4/mul_div.js
@@ -0,0 +1,12 @@
+ const multy=()=>{
+    const num1=document.getElementById("number1").value;
+    const num2=document.getElementById("number2").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+
+};
+
+ const divide=()=>{
+     const num1=document.getElementById("number1").value;
+    const num2=document.getElementById("number2").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+};
