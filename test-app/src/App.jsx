0de912a32c28052fc9a9364e7cb8043556bd8fc5
Pralhadrajparajuli <pralhad.078bct046@acem.edu.np>
2026-09-11 15:42:36 +0545

    explorring react,pops,and components

---
+++ test-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function App() {
+    return (
+        <div>
+            <Header name="Pralhad "  city="pokhara"/>
+            {/* <div>Check</div> */}
+        </div>
+    );
+}
+
+export default App;
---
+++ test-app/src/Header.jsx
@@ -0,0 +1,9 @@
+const Header=(props)=>{
+     return (
+        <div>
+     <h1> helllooooooo {props.name}</h1>
+     <p>City:{props.city}</p>
+     </div>
+    )
+};
+export default Header;
